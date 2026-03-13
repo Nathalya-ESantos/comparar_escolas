ef54f91fdb37e67f36fa87239d41c524fe93e749
nathalya <nathalya.e.santos@aluno.senai.br>
2026-03-13 15:00:04 -0300

    FIZ O QUE DEU

--- comparar-escolas.js
+++ comparar-escolas.js
@@ -12,25 +12,83 @@ class Senai {
     this.statusFuncionamento = false;
   }
 
-    dadosEscola() {
-      console.log(`\n=== RELATÓRIO DE CADASTRO: ${this.cidade.toUpperCase()} ===`);
-      console.log(`- Código: ${this.codigo}`);
-      console.log(`- Fundação: ${this.anoConstruçao} (${this.idade} anos de história)`);
-      console.log(`Capacidade: ${this.qtdeCursos} cursos ativos.`);
-      console.log(`=========================================\n`);
-  }
       /*se status estiver True ele deve abrir a escola */
     abrirEscola(){
         this.statusFuncionamento = true;
-        console.log(`O SENAI: ${this.cidade} está ABERTO`);
+        return `Aberta: Bem-Vindos ao SENAI ${this.cidade}`;
         }
         /*mesma coisa mas com false */
     fecharEscola(){
         this.statusFuncionamento = false;
-        console.log(`O SENAI: ${this.cidade} está FECHADO`);
+        return `Fechada: Atividades encerradas`;
         }
 
   }
+const codigoA = document.querySelector('unidadeA__codigo');
+const cidadeA = document.querySelector('unidadeA__cidade');
+const anoConstrucaoA = document.querySelector('unidadeA__ano');
+const qtdeCursosA = document.querySelector('unidadeA__cursos');
+
+const codigoB = document.querySelector('unidadeB__codigo');
+const cidadeB = document.querySelector('unidadeB__cidade');
+const anoConstrucaoB = document.querySelector('unidadeB__ano');
+const qtdeCursosB = document.querySelector('unidadeB__cursos');
+
+const instanciarA = document.querySelector('unidadeA__instanciar');
+const instanciarB = document.querySelector('unidadeB__instanciar');
+
+const abrirA = document.querySelector('unidadeA__abrir')
+const fecharA = document.querySelector('unidadeA__fechar')
+const abrirB = document.querySelector('unidadeB__abrir')
+const fecharB =document.querySelector('unidadeB__abrir')
+
+const  compararBtn = document.querySelector('.sistema_comparar')
+const  resetBtn = document.querySelector('.sistema_reset')
+
+const  mensagemA = document.querySelector('.unidadeA_mensagem')
+const  mensagemB = document.querySelector('.unidadeB_mensagem')
+
+const relatorio = document.querySelector('.relatorio')
+const veredito = document.querySelector('.relatorio_veredito')
+const linhaA = document.querySelector('.relatorio__linha--a')
+const linhaB = document.querySelector('.relatorio__linha--b')
+
+let escolaA = null;
+let escolab = null;
+
+function validarCampos(cidade,codigo,ano, cursos){
+
+  if{
+    cidade.value === "" ||
+    codigo.value ==="" ||
+    ano.value ===""||
+    cursos.value ===""
+  } {
+    return false;
+    }
+
+    return true;
+};
+
+/*instanciar*/
+
+instanciarA.addEventListener("click", () => {
+  if (validarCampos(cidadeA, codigoA, anoA, cursosA)){
+     mensagemA.textContent = "Preencha todos os campos para criar a escola";
+     return;
+  }
+
+//   escolaA = new Senai {
+//   Number(codigoA.value),
+  
+//   };
+// });
+
+
+
+
+
+
   
     /*COMPARAÇÃO ENTRE ESCOLAS*/
     const compararEsc = (e1, e2) => {
@@ -38,7 +96,7 @@ class Senai {
 
       if (e1.cursos > e2.cursos) {
           console.log(`resultado: ${e1.cidade} possui mais cursos`); /*se a escola 1 possuir mais cursos*/
-      }else if (e2.cursos > e1.cursos)/*a esocla 2 tiver mais*/{
+      }else if (e2.cursos > e1.cursos)/*a escola 2 tiver mais*/{
           console.log(`resultado: ${e2.cidade} possui mais cursos`);
       } else{
           console.log(`resultado: As duas unidades possuem a mesma quantia de cursos`)
@@ -48,7 +106,7 @@ class Senai {
 
     /* Criando instâncias */
     const escolaA = new Senai (603, 'Araraquara', 1998, 60, false);
-    const escolaB = new Senai (603, 'São Carlos', 2000, 120, false);
+    const escolaB = new Senai (201, 'São Carlos', 2000, 120, false);
     
     // execução
     
@@ -57,7 +115,7 @@ class Senai {
 
 /*para passar as informações:*/
 const escola1 = new Senai(603, 'Araraquara', '1998', '60');
-const escola2 = new Senai(102, 'São Carlos', '2000', '120');
+const escola2 = new Senai(201, 'São Carlos', '2000', '120');
 
 escola1.dadosEscola();
 escola2.dadosEscola();
